42bb202a023f47a1ca201a481251afd8261769d7
Roman <roman-thiele@web.de>
2011-06-24 00:57:35 +0200

    Fixed wrong calculation in DipoleField

--- basicfields.h
+++ basicfields.h
@@ -11,19 +11,19 @@ class DipoleField : public Bfield
 {
 	public:
 		DipoleField(Basetracking *t, const Threevector m, const Threevector pos) : 
-			Bfield(t), m(m), dr(-pos), r2(pos.magsquare()), r5(r2*r2*pos.mag())  {};
+			Bfield(t), m(m), dr(-pos) {};
 		Threevector eval(const double time) const {
 			/// r is set to the vector connecting the position of the dipole x0 with the
 			/// current position of the particle x.
 			/// \f[ \vec r = \vec x - \vec x_0 \f]
 			Threevector r = fTracker->getPosition(time) + dr;
+			const double r2 = r.magsquare();
+			const double r5 = r2*r2*r.mag();
 			// 1e-7 = mu0 / 4pi
 			return (1e-7/r5) * (3*r*(m*r)-m*r2);
 		};
 	private:
 		const Threevector m, dr;
-		const double r2; //< r^2
-		const double r5; //< r^5
 
 };
 
